922e2d55edc646ec639a9eaf6450f545f83a9aa2
Colin McFaul <colinjmcfaul@gmail.com>
2022-05-20 16:48:39 -0400

    Update git_redeploy.sh

--- git_redeploy.sh
+++ git_redeploy.sh
@@ -4,10 +4,10 @@ git_status=$(git pull origin main)
 
 echo $git_status 
 
-if [ "$git_status"==*"Already up-to-date."* ]; then
-  echo "Up to date with git repository"
+if [ "$git_status" == *"Already up-to-date."* ]; then
+  echo "Up-to-date with git repository"
   exit 
-elif ["$git_status"==*"Updating"*]; then 
+elif ["$git_status" == *"Updating"*]; then 
   echo "Updating App and redeploying"
 else
   echo "Unable to pull new version from git"
